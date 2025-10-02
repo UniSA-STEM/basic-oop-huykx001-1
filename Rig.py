f1c98d896afd0393cc770d68bdab8debb7946f5b
huykx001 <huykx001@mymail.unisa.edu.au>
2025-10-02 23:03:33 +0930

    Created attributes for the class Rig.

--- Rig.py
+++ Rig.py
@@ -1,8 +1,24 @@
 """
 File: Rig.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: This module displays the rig(computer) class which has methods
+that...
+Author: Kim Xuyen Huynh
+ID: 110442620
+Username: HUYKX001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Rig:
+    def __init__(self, name):
+        # Constructor for the class Rig to initialise the variables.
+        self.__name = name
+        self.__damage_counter = 0
+        self.__broken_state = False
+        self.__storage = []
+        self.__data_spikes = 2
+        self.__encrypted_drive = 1
+        self.__upgrade_level = 0
+
+    # String conversation method to format Rig.
+    def __str__(self):
+        pass
