fc606a1b689183cba6c855b15c88adf83cfe5858
huykx001 <huykx001@mymail.unisa.edu.au>
2025-10-02 22:58:18 +0930

    Created attributes for the class Hacker.

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,21 @@
 """
 File: Hacker.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: This module represents the hacker class and has methods
+that allow the hacker to...
+Author: Kim Xuyen Huynh
+ID: 110442620
+Username: HUYKX001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    def __init__(self, name):
+        # Constructor for the class Hacker to initialise the variables.
+        self.__name = name
+        self.__inventor = []
+        self.__rig = None
+        self.__trace_level = 0
+
+    # String conversation method to format Hacker.
+    def __str__(self):
+        pass
