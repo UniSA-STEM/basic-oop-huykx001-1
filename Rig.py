05918f1eeee155c228d1b8ab312c12a673f46444
huykx001 <huykx001@mymail.unisa.edu.au>
2025-10-15 22:20:49 +1030

    Created getters for the private attributes in class Rig

--- Rig.py
+++ Rig.py
@@ -20,6 +20,28 @@ class Rig:
         self.__removable_drive = 1
         self.__upgrade_level = 0
 
+    # Getters for the private attribute
+    def get_name(self):
+        return self.__name
+
+    def get_damage_counter(self):
+        return self.__damage_counter
+
+    def get_broken_state(self):
+        return self.__broken_state
+
+    def get_storage(self):
+        return self.__storage
+
+    def get_data_spikes(self):
+        return self.__data_spikes
+
+    def get_removable_drive(self):
+        return self.__removable_drive
+
+    def get_upgrade_level(self):
+        return self.__upgrade_level
+
     # String conversation method to format Rig.
     def __str__(self):
         pass
