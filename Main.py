0a8864e7c940c084cbbf8c748aab017fa0a3505f
huykx001 <huykx001@mymail.unisa.edu.au>
2025-10-17 16:35:29 +1030

    Imported the modules, instantiated the objects, and created some testing

--- Main.py
+++ Main.py
@@ -1,8 +1,29 @@
 """
 File: main.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Kim Xuyen Huynh
+ID: 110442620
+Username: HUYKX001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from Hacker import Hacker
+from Asset import Asset
+from Rig import Rig
+
+# names created for hacker and rig
+hacker = Hacker("Anonymous")
+
+rig = Rig("Anomaly")
+foe_rig = Rig("Blank")
+
+# acquires rig
+hacker.acquire_rig(rig)
+
+# attacks opponent rig
+hacker.launch_data_spikes(foe_rig)
+
+# repair Rig
+
+
+# print(hacker)
