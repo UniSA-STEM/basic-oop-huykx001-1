0772ba307bcd48e218a5d059af25a8bcba406b9e
huykx001 <huykx001@mymail.unisa.edu.au>
2025-10-20 02:20:06 +1030

    created different testing scenarios under each comment

--- Main.py
+++ Main.py
@@ -14,50 +14,64 @@ from Rig import Rig
 # names created for hacker and rig
 hacker = Hacker("Anonymous")
 rig = Rig("Anomaly")
-
 # name created for opponent rig
 foe_rig = Rig("Blank")
 
-# # acquiring another rig without a CryptoToken
-# hacker.acquire_rig(rig)
-# hacker.acquire_rig(rig)
+## testing here
+# acquiring another rig without a CryptoToken
+hacker.acquire_rig(rig)
+hacker.acquire_rig(rig)
 
 # hacker acquiring rig and attacking opponent
-# hacker.acquire_rig(rig)
-#
-# hacker.launch_data_spikes(foe_rig)
-# hacker.launch_data_spikes(foe_rig)
-#
-# print(hacker)
-# print(rig)
-# print(foe_rig)
+hacker.acquire_rig(rig)
 
+hacker.launch_data_spikes(foe_rig)
+hacker.launch_data_spikes(foe_rig)
 
+print(hacker)
+print(rig)
+print(foe_rig)
 
+# Not enough data spikes to launch attack
+hacker.acquire_rig(rig)
 
+hacker.launch_data_spikes(foe_rig)
+hacker.launch_data_spikes(foe_rig)
+hacker.launch_data_spikes(foe_rig)
 
-# # upgrade rig
-# rig.upgrade_rig(hacker)
-# rig.upgrade_rig(hacker)
-# rig.upgrade_rig(hacker)
-#
-# # attacks opponent rig
-#
-# # repair Rig
-# # foe_rig.repair_rig()
-#
-# # encrypts assets
-# hacker.encrypt_assets()
-#
-# # removes assets and transfer into inventory
-# rig.store_release_assets(hacker, "RemovableDrive")
-#
-# # removes assets and transfer into storage
-# hacker.store_retrieve_assets(rig, "HardwarePatch")
-#
-# # string representation of class Hacker & Rig
-# print(hacker)
-# print(rig)
-#
-# # string representation of opponent
-# print(foe_rig)
+# repairs rig when broken
+hacker.acquire_rig(rig)
+hacker.launch_data_spikes(foe_rig)
+hacker.launch_data_spikes(foe_rig)
+foe_rig.repair_rig()
+
+# upgrades rigs
+hacker.acquire_rig(rig)
+rig.upgrade_rig(hacker)
+rig.upgrade_rig(hacker)
+rig.upgrade_rig(hacker)
+
+# encrypts assets
+hacker.encrypt_assets()
+
+# removes assets and transfer into Hacker's inventory
+hacker.acquire_rig(rig)
+hacker.launch_data_spikes(foe_rig)
+hacker.encrypt_assets()
+rig.store_release_assets(hacker, "RemovableDrive") # test out other assets here
+print(hacker)
+print(rig)
+
+# removes assets and transfer into Rig's storage
+hacker.acquire_rig(rig)
+hacker.launch_data_spikes(foe_rig)
+hacker.encrypt_assets()
+hacker.store_retrieve_assets(rig, "HardwarePatch") # test out other assets here
+print(hacker)
+print(rig)
+
+# test for string conversion method to work
+hacker.acquire_rig(rig)
+print(hacker)
+print(rig)
+print(foe_rig)
