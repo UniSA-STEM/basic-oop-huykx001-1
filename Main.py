3a42a96944693adafd5952d761eba8004ff7de17
huykx001 <huykx001@mymail.unisa.edu.au>
2025-10-20 01:31:42 +1030

    created testing for acquiring rigs with/without cryptotoken and attacking the opponent.

--- Main.py
+++ Main.py
@@ -13,33 +13,51 @@ from Rig import Rig
 
 # names created for hacker and rig
 hacker = Hacker("Anonymous")
-
 rig = Rig("Anomaly")
+
+# name created for opponent rig
 foe_rig = Rig("Blank")
 
-# acquires rig
-hacker.acquire_rig(rig)
+# # acquiring another rig without a CryptoToken
+# hacker.acquire_rig(rig)
+# hacker.acquire_rig(rig)
 
-# upgrade rig
-rig.upgrade_rig(hacker)
-rig.upgrade_rig(hacker)
-rig.upgrade_rig(hacker)
+# hacker acquiring rig and attacking opponent
+# hacker.acquire_rig(rig)
+#
+# hacker.launch_data_spikes(foe_rig)
+# hacker.launch_data_spikes(foe_rig)
+#
+# print(hacker)
+# print(rig)
+# print(foe_rig)
 
-# attacks opponent rig
-hacker.launch_data_spikes(foe_rig)
-hacker.launch_data_spikes(foe_rig)
 
-# repair Rig
-# foe_rig.repair_rig()
 
-hacker.encrypt_assets(hacker)
 
-# removes assets and transfer into inventory
-rig.store_release_assets(hacker, "RemovableDrive")
 
-# string representation of class Hacker & Rig
-print(hacker)
-print(rig)
-
-# string representation of opponent
-print(foe_rig)
+# # upgrade rig
+# rig.upgrade_rig(hacker)
+# rig.upgrade_rig(hacker)
+# rig.upgrade_rig(hacker)
+#
+# # attacks opponent rig
+#
+# # repair Rig
+# # foe_rig.repair_rig()
+#
+# # encrypts assets
+# hacker.encrypt_assets()
+#
+# # removes assets and transfer into inventory
+# rig.store_release_assets(hacker, "RemovableDrive")
+#
+# # removes assets and transfer into storage
+# hacker.store_retrieve_assets(rig, "HardwarePatch")
+#
+# # string representation of class Hacker & Rig
+# print(hacker)
+# print(rig)
+#
+# # string representation of opponent
+# print(foe_rig)
